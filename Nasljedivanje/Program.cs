8b4e07382b8d207663d1df442b711765dea67808
Pat <113007904+PattZac@users.noreply.github.com>
2022-11-09 09:26:37 +0100

    Add files via upload

---
+++ Nasljedivanje/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedivanje
+{
+    class Cat
+    {
+        private bool opasna;
+        public bool Opasna { get => opasna; set => opasna = value; }
+        public virtual void Opasnost()
+        {
+            this.opasna = false;
+        }
+        public override string ToString()
+        {
+            return "Maca - opasnost = " + this.opasna + ".";
+        }
+    }
+    class Gepard : Cat
+    {
+        public override void Opasnost()
+        {
+            this.Opasna = true;
+        }
+        public override string ToString()
+        {
+            return "Gepard - opasnost = "+this.Opasna+".";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+            g.Opasnost();
+            Console.WriteLine(g.ToString());
+            Cat c = new Cat();
+            c.Opasnost();
+            Console.WriteLine("\r\n"+c.ToString());
+            Console.ReadKey();
+        }
+    }
+}
